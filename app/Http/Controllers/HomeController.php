5028e3b627c3303778de6c03b7c8e727afe23ada
AungPhyoKywe <kophyokywe07@gmail.com>
2019-05-20 16:50:20 +0800

    edit dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,23 +26,25 @@ class HomeController extends Controller
      */
     public function index()
     {
-$id=Auth::user()->company_id;
-$joblist=job::where('company_id',$id)->where('status',1)->get();
-$users=User::where('role_id',3)->get();
- 
+    $id=Auth::user()->company_id;
+    $joblist=job::where('company_id',$id)->where('status',1)->get();
+    $users=User::where('role_id',3)->get();
+    $owner=User::where('role_id',2)->get();
+    $u=Auth::user()->company_id;
+    $applylist= DB::table('job_aplied')->select('users.name','job.job_name','users.pic_path','job_aplied.cv','job.status','job.job_id','job_aplied.created_at','job_aplied.jobapp_id')->join('users','users.user_id','=','job_aplied.user_id')->join('job','job.job_id','=','job_aplied.job_id')->where('job_aplied.status',1)->where('job.company_id',$u)->get();
+    $wordCount = $joblist->count();
+    $seekerCount=$users->count();
+    $applylistCount=$applylist->count();
+    $ownerlist=$owner->count();
+return view('back-end.dashboard', compact('wordCount', 'seekerCount', 'applylistCount','ownerlist'));
+        // return view('',
 
-$wordCount = $joblist->count();
-$seekerCount=$users->count();
 
+        //     ["wordCount"=>$wordCount],
 
-        return view('back-end.dashboard',
+        //     ["seekerCount"=>$seekerCount]
 
-
-            ["wordCount"=>$wordCount],
-
-            ["seekerCount"=>$seekerCount]
-
-        );
+        // );
     }
 
     
--- resources/views/back-end/dashboard.blade.php
+++ resources/views/back-end/dashboard.blade.php
@@ -259,7 +259,7 @@
             
 
             <!-- Earnings (Monthly) Card Example -->
-            <div class="col-md-4 mb-4">
+            <div class="col-md-6 mb-4">
               <div class="card border-left-success shadow h-100 py-2">
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
@@ -280,41 +280,16 @@
             </div>
 
             <!-- Earnings (Monthly) Card Example -->
-            <div class="col-md-4 mb-4">
-              <div class="card border-left-info shadow h-100 py-2">
-                <div class="card-body">
-                  <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
-                        Jobs Applied list
-                      </div>
-                      <div class="row no-gutters align-items-center">
-                        <div class="col-auto">
-                          <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">12</div>
-                        </div>
-                        <div class="col">
-                          <div class="progress progress-sm mr-2">
-                            <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-auto">
-                      <i class="fas fa-user fa-2x text-gray-300"></i>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            
 
             <!-- Pending Requests Card Example -->
-            <div class="col-md-4 mb-4">
+            <div class="col-md-6 mb-4">
               <div class="card border-left-warning shadow h-100 py-2">
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
                       <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Jobs Owner List</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">12</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">{{$ownerlist}}</div>
                     </div>
                     <div class="col-auto">
                       <i class="fas fa-user fa-2x text-gray-300"></i>
@@ -383,7 +358,7 @@
                       <div class="row no-gutters align-items-center">
                         <div class="col-auto">
                           <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
-                            12
+                           {{$applylistCount}}
 
                           </div>
                         </div>
